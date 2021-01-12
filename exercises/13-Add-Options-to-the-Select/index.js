d77d585aca567c7cc1adc26b3efa42042ba676f9
pilarguz10 <pilar1494@hotmail.com>
2021-01-12 19:01:19 +0000

    Hecho hasta el 14

--- exercises/13-Add-Options-to-the-Select/index.js
+++ exercises/13-Add-Options-to-the-Select/index.js
@@ -2,5 +2,22 @@ window.onload = function() {
 	console.log("The website was loaded...");
 	let countries = ["USA", "France", "Italy", "Brazil", "Colombia", "Belize", "Venezuela"];
 
-  // your code here
+	// your code here
+    for (let i = 0; i < countries.length; i++) 
+    {
+        //Guardo en una variable el elemento del DOM Select
+        let element = document.getElementById("mySelect");
+        //Guardo en una variable el nuevo elemento que creo opción
+		let elementOption = document.createElement("option");
+
+        //Pinto el valor de countries en cada opción 
+        elementOption.innerHTML = countries[i];
+        //Agrego la opción al select del DOM
+		element.appendChild(elementOption);
+	}
+
+	//Escucho  cuando en el select se provoca el evento change y saco la alerta
+	document.getElementById("mySelect").addEventListener("change", function() {
+		alert(this.value);
+	});
 };
--- exercises/14-Todo-List/index.js
+++ exercises/14-Todo-List/index.js
@@ -1,1 +1,30 @@
 // Your code here
+
+//Crear un nuevo elemento
+// function crearNuevoLi() {
+// 	let newLi = document.createElement("li");
+// 	let valorinput = document.getElementById("myInput");
+// 	var t = document.createTextNode(valorinput);
+
+// 	newLi.appendChild(t);
+// 	if (valorinput === "") {
+// 		alert("You must write something!");
+// 	} else {
+// 		document.getElementById("addToDo").appendChild(li);
+// 	}
+
+// 	document.getElementById("myInput").value = "";
+// }
+
+function newElement() {
+	var li = document.createElement("li");
+	var inputValue = document.getElementById("addToDo").value;
+	var t = document.createTextNode(inputValue);
+	li.appendChild(t);
+	if (inputValue === "") {
+		alert("You must write something!");
+	} else {
+		document.getElementById("myUL").appendChild(li);
+	}
+	document.getElementById("addToDo").value = "";
+}
